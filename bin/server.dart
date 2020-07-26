49e843775f7fdf8ebd2360e5d7abd30180f09f6b
Lukas Renggli <renggli@gmail.com>
2020-07-26 15:50:11 +0200

    Add a router to serve multiple applications

--- bin/server.dart
+++ bin/server.dart
@@ -2,12 +2,18 @@ library seaside.bin.server;
 
 import 'package:seaside/seaside.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
 
+import 'counter.dart';
+import 'multi_counter.dart';
 import 'tabbed_counters.dart';
 
-/// Starts the multi-counter application.
 Future<void> main(List<String> args) async {
-  final server =
-      await serve(Application((request) => TabbedCounter()), 'localhost', 8080);
+  final router = Router();
+  router.all('/counter', Application((request) => Counter()));
+  router.all('/multi-counter', Application((request) => MultiCounter()));
+  router.all('/tabbed-counter', Application((request) => TabbedCounter()));
+
+  final server = await serve(router.handler, 'localhost', 8080);
   print('Serving at http://${server.address.host}:${server.port}');
 }
