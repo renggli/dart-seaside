774cbab0fc9badd2d70d50ffe60b854db30ed17e
Lukas Renggli <renggli@gmail.com>
2021-11-14 16:18:08 +0100

    Improve the dialogs

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -3,11 +3,11 @@ import 'component.dart';
 import 'continuation.dart';
 
 /// Simple dialog that shows a title and contents.
-class Dialog extends Component with CanAnswer<void> {
+class Dialog extends Component with CanAnswer<Never> {
+  Dialog({this.title = '', this.message = ''});
+
   final String title;
   final String message;
-
-  Dialog({this.title = '', this.message = ''});
 
   @override
   String body(Continuation continuation) => '<h1>$title</h1><p>$message</p>';
@@ -15,10 +15,10 @@ class Dialog extends Component with CanAnswer<void> {
 
 // Simple dialog with an ok button.
 class AlertDialog extends Component with CanAnswer<void> {
+  AlertDialog({this.title = '', this.message = ''});
+
   final String title;
   final String message;
-
-  AlertDialog({this.title = '', this.message = ''});
 
   @override
   String body(Continuation continuation) {
@@ -32,10 +32,10 @@ class AlertDialog extends Component with CanAnswer<void> {
 
 // Simple dialog with yes and no button.
 class ConfirmationDialog extends Component with CanAnswer<bool> {
+  ConfirmationDialog({this.title = '', this.message = ''});
+
   final String title;
   final String message;
-
-  ConfirmationDialog({this.title = '', this.message = ''});
 
   @override
   String body(Continuation continuation) {
@@ -51,19 +51,18 @@ class ConfirmationDialog extends Component with CanAnswer<bool> {
 
 // Simple dialog that allows entering a string.
 class InputDialog extends Component with CanAnswer<String> {
+  InputDialog({this.title = '', this.message = '', this.value = ''});
+
   final String title;
   final String message;
   final String value;
 
-  InputDialog({this.title = '', this.message = '', this.value = ''});
-
   @override
   String body(Continuation continuation) {
     final id = continuation.callbackKey(answer);
-    return '<h1>$title</h1>'
+    return '<h1>$title</h1><p>$message</p>'
         '<form action="${continuation.actionUrl()}" method="post">'
-        '<label for="$id">$message</label><br/>'
-        '<input type="text" id="$id" name="$id" value="$value"><br/>'
+        '<input type="text" name="$id" value="$value"><br/>'
         '<input type="submit" value="Ok">'
         '</form>';
   }
@@ -71,22 +70,32 @@ class InputDialog extends Component with CanAnswer<String> {
 
 /// Simple dialog that allows picking an item from a list.
 class PickerDialog<T> extends Component with CanAnswer<T> {
-  final String title;
-  final List<T> values;
+  PickerDialog(
+      {this.title = '',
+      this.message = '',
+      this.selected,
+      this.values = const []});
 
-  PickerDialog({this.title = '', this.values = const []});
+  final String title;
+  final String message;
+  final T? selected;
+  final List<T> values;
 
   @override
   String body(Continuation continuation) {
     final buffer = StringBuffer();
-    buffer.write('<h1>$title</h1>');
-    buffer.write('<ul>');
-    for (final value in values) {
-      buffer.write('<li>'
-          '<a href="${continuation.actionUrl(() => answer(value))}">$value</a>'
-          '</li>');
+    buffer.write('<h1>$title</h1><p>$message</p>');
+    buffer.write('<form action="${continuation.actionUrl()}" method="post">');
+    final callback =
+        continuation.callbackKey((value) => answer(values[int.parse(value)]));
+    for (var i = 0; i < values.length; i++) {
+      final value = values[i], id = '$callback-$i';
+      buffer.write('<input type="radio" id="$id" name="$callback" '
+          'value="$value" ${selected == value ? 'checked' : ''}>');
+      buffer.write('<label for="$id">$value</label><br/>');
     }
-    buffer.write('</ul>');
+    buffer.write('<input type="submit" value="Ok">');
+    buffer.write('</form>');
     return buffer.toString();
   }
 }
